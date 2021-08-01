c0009093ff687feff0ffff2f3f77d0d728c0803a
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-08-01 14:52:31 -0500

    First Commit

---
+++ premake5.lua
@@ -0,0 +1,78 @@
+workspace "Server"
+	architecture "x86_64"
+	startproject "server"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+include "vendor/glfw"
+include "vendor/glad"
+
+project "Server"
+	location "server"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+	
+	pchheader "vkpch.h"
+	pchsource "server/src/vkpch.cpp"
+
+	targetdir ("bin")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"server/src/**.h",
+		"server/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"engine/src",
+		"vendor/glfw/include"
+	}
+
+	links 
+	{ 
+		"glfw"
+	}
+
+	defines
+	{
+		"GLFW_INCLUDE_NONE"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		
+		defines
+		{
+			"VE_WIN"
+		}
+
+	filter "configurations:Debug"
+		defines "VE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "HVE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "VE_DIST"
+		runtime "Release"
+		optimize "on"
